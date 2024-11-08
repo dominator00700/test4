9a8754f74e2d24b2b166701120150e29874cbebd
dominator00700 <dominatordlk@gmail.com>
2024-11-08 19:55:37 +0100

    first commit

---
+++ not-found.tsx
@@ -0,0 +1,16 @@
+
+// src/app/podmienky/page.tsx
+
+
+import Typography  from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function NotFound() {
+
+    return (  
+        <Box>
+          <Typography> ERROR 404 STRÁNKA SA NENAŠLA</Typography>
+        </Box>
+  );
+} 
+
